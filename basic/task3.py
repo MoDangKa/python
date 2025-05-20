8970f0229f6516c2760d4a7f92bbfb38a3a3a34c
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2025-05-20 10:01:53 +0700

    refactor: update code

--- basic/task3.py
+++
@@ -1,8 +0,0 @@
-states_of_america = ["Delaware", "Pennsylvania"]
-
-states_of_america[1] = "Pencilvania"
-
-# states_of_america.append("Angelaland")
-states_of_america.extend(["Angelaland", "Jack Bauer Land"])
-
-print(states_of_america)
